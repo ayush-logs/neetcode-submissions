e9034ad75fe0811f6fe24497d260a90b6c35c6d6
ayush <ayush.devhub@gmail.com>
2026-04-17 11:27:02 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        """
+        O(n) O(d)
+        d = distinct elements in Counter
+        """
+        # return Counter(nums).most_common(1)[0][0]
+
+        """
+        Boyer Moore Voting Algorithm
+        O(n) O(1)
+        """
+        maj = nums[0]
+        freq = 1
+
+        for i in range(1, len(nums)): 
+            if nums[i] != maj: 
+                freq -= 1
+                if freq == 0: 
+                    maj = nums[i]
+                    freq = 1
+            else: 
+                freq += 1
+        
+        return maj
+        
+        
